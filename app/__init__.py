1ae46659773454a23b222f30a9bbd4c3ec736685
Ditthaphong Jaratchairot <ditthaphong@syssteel.com>
2024-12-12 13:45:04 +0700

    Change Database to Azure By P'Tae

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,3 @@
-# from flask import Flask
-# from flask_restx import Api
-# from flask_sqlalchemy import SQLAlchemy
-# from app.routes import box_ns
-# from app.config import Config
-
-# # db = SQLAlchemy()  # ใช้ตัวเดียวกับใน models.py
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     # # ตั้งค่าการเชื่อมต่อกับฐานข้อมูล
-#     # app.config.from_object(Config)  # โหลดการตั้งค่าจาก Config
-
-#     # db.init_app(app)  # เชื่อมโยง db กับ app
-
-#     api = Api(app, version='1.0', title='SYS DocFinder', description='API for Locate Document')
-
-#     # Register blueprint for routes
-#     api.add_namespace(box_ns)
-
-#     return app
-
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
--- app/config.py
+++ app/config.py
@@ -1,5 +1,16 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://webac:webac@1syscmpsy05/WEBAC?driver=ODBC+Driver+17+for+SQL+Server'
-    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # ODBC connection string for Azure SQL Database
+    DRIVER = "{ODBC Driver 18 for SQL Server}"
+    SERVER = "tcp:siamyamato.database.windows.net"
+    DATABASE = "thoth_datawarehouse"
+    UID = "administratorsys"
+    PWD = "DxIT$y$2023"
+    ENCRYPT = "yes"
+    TrustServerCertificate = "no"
+    TIMEOUT = 30
+    # SQLAlchemy Database URI
+    SQLALCHEMY_DATABASE_URI = (
+        f"mssql+pyodbc://{UID}:{PWD}@{SERVER}/{DATABASE}?driver={DRIVER}&Encrypt={ENCRYPT}&TrustServerCertificate={TrustServerCertificate}&timeout={TIMEOUT}"
+    )
