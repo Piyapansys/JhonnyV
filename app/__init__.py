91c512e5f2b903231189c832eddd0814130a572d
SYSC\ditthaphong <miimicc.mix@gmail.com>
2025-10-10 10:05:55 +0700

    cors fixed

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     
     # Configure CORS to allow requests from frontend
     CORS(app, 
-         origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000"],
+         origins=["*"],
          methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
          allow_headers=["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
          supports_credentials=True,
